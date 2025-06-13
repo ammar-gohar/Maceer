b321b54d604c31266a6271364c1f3f69bbdc01fd
Sefein <sefansaid@gmail.com>
2025-06-13 17:46:03 +0300

    Typo

--- resources/views/mails/sending-password.blade.php
+++ resources/views/mails/sending-password.blade.php
@@ -2,7 +2,7 @@
 # Your Schoolary account
 
 Hello, {{ $name }}
-Your account on Schoolary is now ready with passord <span style="background: lightgray; padding: 0.25rem 0.5rem;">{{ $password }}</span>.
+Your account on Schoolary is now ready with password <span style="background: lightgray; padding: 0.25rem 0.5rem;">{{ $password }}</span>.
 Once you login you can change your password as you wish!
 
 {{-- <x-mail::button :url="$url">
