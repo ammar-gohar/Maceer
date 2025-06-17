37b4f9258e951c3ee671df1902f9379a0c7c0a40
AG2k2 <ammarnagy369@gmail.com>
2025-06-17 23:29:42 +0300

    sdfas

--- app/View/Components/page.php
+++ app/View/Components/page.php
@@ -9,18 +9,25 @@ use Illuminate\View\Component;
 class page extends Component
 {
 
+    public $show_index_button = false;
+    public $show_create_button = false;
+    public $show_edit_button = false;
+    public $show_delete_button = false;
+
+    public $title = '';
+    public $module = '';
+
+
     /**
      * Create a new component instance.
      */
-    public function __construct(
-        public $show_index_button = false,
-        public $show_create_button = false,
-        public $show_edit_button = false,
-        public $show_delete_button = false,
-
-        public $title = '',
-        public $module = '',
-    ){}
+    public function __construct($show_index_button = false, $show_create_button = false, $show_edit_button = false, $show_delete_button = false, $title = '', $module = ''){
+        $this->module = $module;
+        $this->title = $title;
+        $this->show_index_button = $show_index_button;
+        $this->show_edit_button = $show_edit_button;
+        $this->show_create_button = $show_create_button;
+    }
 
     /**
      * Get the view / contents that represent the component.
