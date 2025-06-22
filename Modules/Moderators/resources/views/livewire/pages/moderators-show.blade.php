ca3cf1015216cc82ade5c178c9cb2b786da9879f
AG2k2 <ammarnagy369@gmail.com>
2025-06-23 00:15:18 +0300

    images show

--- Modules/Moderators/resources/views/livewire/pages/moderators-show.blade.php
+++ Modules/Moderators/resources/views/livewire/pages/moderators-show.blade.php
@@ -3,6 +3,13 @@
     <!--begin::Body-->
     <div class="px-4 card-body">
 
+        <div class="mb-3 row">
+            <h5 class="col-sm-3 col-form-label fs-5">@lang('forms.image')</h5>
+            <div class="col-sm-9 d-flex">
+                <img src="{{ asset($moderator->image ? 'storage/' . $moderator->image : 'favicon.png') }}" alt="User image" class="img-thumbnail" width="120" height="90">
+            </div>
+        </div>
+
         <livewire:components.show-item label="forms.name" :data="$moderator->full_name" />
 
         <livewire:components.show-item label="forms.national_id" :data="$moderator->national_id" />
