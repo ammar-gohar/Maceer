3ed49cc938a343017d6fdcea15d22cef78599183
Sefein <sefansaid@gmail.com>
2025-07-05 18:53:22 +0300

    make emails valid

--- Modules/Students/app/Livewire/Pages/StudentEdit.php
+++ Modules/Students/app/Livewire/Pages/StudentEdit.php
@@ -73,10 +73,10 @@ class StudentEdit extends Component
 
         $email = new \SendGrid\Mail\Mail(); 
         $email->setFrom("info@maceer.systems", "Maceer admin");
-        $email->setSubject();
+        $email->setSubject("New user password");
         $email->addTo($this->form->email, $this->form->first_name . ' ' . $this->form->last_name);
         $email->addContent(
-            "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+            "text/html", "here is your password <strong> $password </strong>"
         );
         $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
         try {
