bd4b43edbd967ccc0f7c0f24a1d8e8a88813352e
AG2k2 <ammarnagy369@gmail.com>
2025-07-06 23:40:44 +0300

    test 2

--- Modules/Courses/resources/views/livewire/pages/course-requests.blade.php
+++ Modules/Courses/resources/views/livewire/pages/course-requests.blade.php
@@ -32,7 +32,7 @@
             </div>
             <div class="mb-2 row">
                 <h6 class="col-6">@lang('modules.students.guide'): {{ Auth::user()->student->guide->first_name . ' ' . Auth::user()->student->guide->last_name }}</h6>
-                <h6 class="text-danger col-6">@lang('modules.courses.enrollment_end'):</h6>
+                <h6 class="text-danger col-6">@lang('modules.courses.enrollment_end'): {{ $enroll_end }}</h6>
             </div>
         </div>
         <!--begin::Form-->
