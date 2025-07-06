23cb6470050172457ce365476325cc512f65d4aa
AG2k2 <ammarnagy369@gmail.com>
2025-07-07 02:06:29 +0300

    test 2

--- Modules/Courses/resources/views/livewire/professor-course-student-list.blade.php
+++ Modules/Courses/resources/views/livewire/professor-course-student-list.blade.php
@@ -6,13 +6,13 @@
         {{ $enroll->student->full_name }}
     </td>
     <td>
-        <input class="form-control" type="number" name="midterm" id="midterm" wire:model.blur='midterm' min="0" step="0.01">
+        <input class="form-control" type="number" name="midterm" id="midterm" wire:model.live='midterm' min="0" step="0.01">
     </td>
     <td>
-        <input class="form-control" type="number" name="work" id="work" wire:model.blur='work' min="0" step="0.01">
+        <input class="form-control" type="number" name="work" id="work" wire:model.live='work' min="0" step="0.01">
     </td>
     <td>
-        <input class="form-control" type="number" name="final" id="final" wire:model.blur='final' min="0" step="0.01">
+        <input class="form-control" type="number" name="final" id="final" wire:model.live='final' min="0" step="0.01">
     </td>
     <td class="text-center">
         {{ $enroll->total_mark }}
