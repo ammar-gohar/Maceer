7ebb79bba822b02446136005acc6388bb208dda6
AG2k2 <ammarnagy369@gmail.com>
2025-07-07 01:27:42 +0300

    test 2

--- Modules/Quizzes/app/Livewire/Pages/QuizzesShowProfessor.php
+++ Modules/Quizzes/app/Livewire/Pages/QuizzesShowProfessor.php
@@ -27,7 +27,7 @@ class QuizzesShowProfessor extends Component
             'score' => array_sum($this->answers->pluck('marks_obtained')->toArray()),
         ]);
 
-        dd($this->marks, $this->answers->pluck('marks_obtained')->toArray(), $this->answer->attemp->score);
+        dd ($this->answers->pluck('marks_obtained')->toArray(), $this->marks, $answer->attempt->score);
 
         notyf()->success(__('modules.quizzes.score_updated.success'));
 
