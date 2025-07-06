11baaae807449ddf441dfba5f0d785659bff0fca
AG2k2 <ammarnagy369@gmail.com>
2025-07-07 02:32:17 +0300

    test 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -85,8 +85,8 @@ class User extends Authenticatable implements MustVerifyEmail
     public function current_enrolled_courses()
     {
         return $this->belongsToMany(Course::class, 'enrollments', 'student_id', 'course_id')
-                    ->withPivot(['semester_id'])
-                    ->wherePivot('semester_id', Semester::where('is_current', 1)->first()->id);
+                    ->whereNotNull('approved_at')
+                    ->whereHas('semester', fn($q) => $q->where('is_current', 1));
     }
 
     // =================================================================================
